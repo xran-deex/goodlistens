ae45abc32d6d314c30a2d35f7847016df0642e19
Randy Valis <randy.valis@gmail.com>
2013-10-04 16:09:53 -0400

    added bootstrap and a navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Goodlistens::Application.routes.draw do
+  get "login/index"
+
   # The priority is based upon order of creation:
   # first created -> highest priority.
 
@@ -48,7 +50,7 @@ Goodlistens::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'application#index'
 
   # See how all your routes lay out with "rake routes"
 
